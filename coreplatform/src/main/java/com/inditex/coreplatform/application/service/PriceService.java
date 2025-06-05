39ac9d556116dccf7579576907fae5c531bbcb04
OmarHoyos12 <yesidhs5@gmail.com>
2025-06-04 20:40:51 -0500

    feat:application-se agrega PriceService y PriceServiceImpl

---
+++ coreplatform/src/main/java/com/inditex/coreplatform/application/service/PriceService.java
@@ -0,0 +1,17 @@
+// coreplatform/src/main/java/com/inditex/coreplatform/application/service/PriceService.java
+package com.inditex.coreplatform.application.service;
+
+import com.inditex.coreplatform.domain.model.Price;
+
+import java.time.LocalDateTime;
+
+/**
+ * Puerto de aplicación: define el caso de uso de obtener precio aplicable.
+ */
+public interface PriceService {
+    /**
+     * Obtiene el precio aplicable para un productId, brandId y fecha dada.
+     * Si no existe, lanza PriceNotFoundException.
+     */
+    Price getApplicablePrice(Long productId, Integer brandId, LocalDateTime applicationDate);
+}
